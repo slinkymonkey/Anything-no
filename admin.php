e2e8c3c6bef6ed511c6d4f014c95ec763ca84d2e
Rajdeep Barman <rbarman@gainsight.com>
2015-04-17 02:30:51 +0530

    made succesful addition to database

--- admin.php
+++ admin.php
@@ -3,11 +3,11 @@
 		<title></title>
 	</head>
 	<body>
-		<form action="admin.php" method="post">
+		<form action="admin.php" method="get">
 			<table>
 				<tr>
 					<td>Name: <input type="text" name="name" value=""/></td>
-					<td>IMDB Rating: <input type="number" name="iRating" value=""/></td>
+					<td>IMDB Rating: <input type="number" step="0.1" name="iRating" value=""/></td>
 					<td>Cast: <input type="text" name="cast" value=""/></td>
 					<td>Year: <input type="number" name="year" value=""/></td>
 					<td><input type="submit" name="submit" value="submit"/></td>
@@ -28,17 +28,27 @@
 		$serverName = 'localhost';
 		$userName = "root";
 		$password = "";
+		$dbname = "mdb";
 	
 	//connecting
-	$conn = mysqli_connect($serverName, $userName, $password);
+	$conn = mysqli_connect($serverName, $userName, $password, $dbname);
 	
 	if (!$conn) {
 		die("Connection failed: ". mysqli_connect_error());
 	}
-	echo "Connection successful";
-	
-		$abc = "insert into movietable(name, imdbRating, cast, year) values(".$_POST['name']."," .$_POST['iRating'].",". $_POST['cast'].",". $_POST['year'].")";
-		mysqli_query($conn, $abc);
+	echo "Connection successful". "\n";
+	$name = $_GET['name'];
+	$rating = $_GET['iRating'];
+	$cast = $_GET['cast'];
+	$year = $_GET['year'];
+	$abc = "insert into movies(name, imdbRating, cast, year) values('".$name."',".$rating.",'".$cast."',".$year.")";
+	echo $abc;
+	try {
+		mysqli_query($conn, $abc);	
+	} catch (Exception $e) {
+		echo 'Error'. $e->getMessage(). "\n";
+	}
+	mysqli_close($conn);
 	}
 	
 	
